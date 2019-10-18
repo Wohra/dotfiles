4be478d06b265dfffd4a638de11d371b8002517d
Wohra <>
2019-10-18 17:24:11 +0200

    Improve polybar.sh startup script

--- .i3/bin/polybar.sh
+++ .i3/bin/polybar.sh
@@ -6,7 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-for bar in "${@}" ; do
-	polybar -r ${bar} &
+# Launch polybar on active monitors
+MONITORS=$(xrandr --listactivemonitors|awk '/ *[0-9]+:/{print $NF}')
+ALL_BARS=$(awk -F'/' '/^\[bar/{print $NF}' ~/.config/polybar/config|sed 's/]//g')
+
+for bar in ${ALL_BARS} ; do
+  for monitor in ${MONITORS} ; do
+    #echo "$bar --> $monitor"
+    #[[ "${bar}" =~ "${monitor}" ]] && polybar -r ${bar}
+    if [[ "${bar}" =~ "${monitor}" ]] ; then
+      polybar -r ${bar} &
+    fi
+  done
 done
